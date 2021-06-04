a2b7f006724fda17b4f2dc669ab555a756f73429
yoshiski <jiubaotianrenxi@gmail.com>
2021-06-04 21:37:18 +0900

    Added views to create/edit blog post inside the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-# from pathlib import Path
+#from pathlib import Path
 # import os
 
 # Django3.2だとsettings.pyの頭
@@ -135,6 +135,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
