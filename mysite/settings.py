2be19408bd7101df6d4799621572852f9afd9cfd
yoshiski <jiubaotianrenxi@gmail.com>
2021-06-09 09:44:14 +0900

    Added views to create/edit blog post inside the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,18 +11,18 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 #from pathlib import Path
-# import os
+import os
 
 # Django3.2だとsettings.pyの頭
 # from pathlib import Path
-# # # Build paths inside the project like this: BASE_DIR / 'subdir'.
+# # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # 2.2.7だと以下になる
-import os
-from pathlib import Path
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# import os
+# from pathlib import Path
+# # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
